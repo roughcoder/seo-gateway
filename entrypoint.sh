c688f5d66b2bf392bc20f61240f891c298675b60
Neil Barton <neil@eat.sleep.dev>
2025-05-06 10:46:06 +0100

    fix spacing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
-    #!/bin/sh
-    set -e # Exit immediately if a command exits with a non-zero status.
+#!/bin/sh
+set -e # Exit immediately if a command exits with a non-zero status.
 
-    echo "Running Prisma migrations..."
-    npx prisma migrate deploy
+npx prisma migrate deploy
 
-    echo "Starting the application..."
-    # Execute the command passed as arguments to this script (which will be the Dockerfile's CMD)
-    exec "$@"
+echo "Starting the application..."
+# Execute the command passed as arguments to this script (which will be the Dockerfile's CMD)
+exec "$@"
